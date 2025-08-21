11e6199eac5c5af724ce364e36528ce8ec58e610
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2025-08-20 21:08:55 -0500

    chore: Bump to version configuration.

--- code_review/__init__.py
+++ code_review/__init__.py
@@ -3,5 +3,6 @@ from code_review.cli import cli
 import code_review.git.main  # This imports the git commands
 import code_review.linting.ruff.main  # This imports the ruff commands
 
+__version__ = "0.1.0"
 if __name__ == "__main__":
     cli()
