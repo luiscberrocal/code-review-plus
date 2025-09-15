5d13df0b18408eb6d188610d82adf542e73105b5
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2025-09-15 08:01:13 -0500

    Added dockerfiles to CodeReviewSchema

--- code_review/review/adapters.py
+++ code_review/review/adapters.py
@@ -5,6 +5,7 @@ from code_review.adapters.changelog import parse_changelog
 from code_review.adapters.setup_adapters import setup_to_dict
 from code_review.coverage.main import get_makefile, get_minimum_coverage
 from code_review.dependencies.pip.handlers import requirements_updated
+from code_review.docker.docker_files.handlers import parse_dockerfile
 from code_review.git.handlers import branch_line_to_dict, check_out_and_pull, get_branch_info
 from code_review.handlers import ch_dir
 from code_review.linting.ruff.handlers import count_ruff_issues
@@ -41,6 +42,14 @@ def build_code_review_schema(folder: Path, target_branch_name: str):
     target_branch.changelog_versions = parse_changelog(folder / "CHANGELOG.md")
     target_branch.requirements_to_update = requirements_updated(folder)
 
+    # Dockerfiles
+    docker_files = list(folder.rglob("Dockerfile"))
+    docker_info_list = []
+    for file in docker_files:
+        docker_info  = parse_dockerfile(file)
+        if docker_info:
+            docker_info_list.append(docker_info)
+
     return CodeReviewSchema(
         name=folder.name,
         source_folder=folder,
@@ -48,6 +57,7 @@ def build_code_review_schema(folder: Path, target_branch_name: str):
         target_branch=target_branch,
         base_branch=base_branch,
         date_created=datetime.now(),
+        docker_files=docker_info_list,
     )
 
 
