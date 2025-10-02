85e1921a7283308d17710a5201e6b02f661f6fa2
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2025-10-02 12:06:43 -0500

    CI rules.

---
+++ code_review/review/rules/gitlab_ci.py
@@ -0,0 +1,21 @@
+from code_review.review.enums import ReviewRuleLevel
+
+
+def check_multiple_targets(ci_data: dict):
+    """Check if there are multiple targets in the CI configuration.
+
+    Args:
+        ci_data: Dictionary containing CI job configurations.
+
+    Returns:
+        True if multiple targets are found, False otherwise.
+    """
+    if not ci_data:
+        return {"level": ReviewRuleLevel.CRITICAL.value, "message": "No CI configuration data found."}
+    for job, conditions in ci_data.items():
+        if isinstance(conditions, list) and len(conditions) > 1:
+            return {"level": ReviewRuleLevel.ERROR.value,
+                    "message": f"We have more than one ci targets ({len(conditions)})."}
+
+    return {"level": ReviewRuleLevel.INFO.value, "message": "CI configuration is Oke."}
+
--- tests/unit/gitlab/ci/test_handlers.py
+++ tests/unit/gitlab/ci/test_handlers.py
@@ -1,8 +1,8 @@
-from code_review.plugins.gitlab.ci.handlers import handle_mult_targets
+from code_review.plugins.gitlab.ci.handlers import handle_multi_targets
 
 
 def test_handle_mult_targets(fixtures_folder) -> None:
-    result = handle_mult_targets(fixtures_folder, "gitlab-ci.yml")
+    result = handle_multi_targets(fixtures_folder, "gitlab-ci.yml")
 
     assert result is not None, "The function should return a dictionary for a valid .gitlab-ci.yml file."
     assert isinstance(result, dict), "The function should return a dictionary."
