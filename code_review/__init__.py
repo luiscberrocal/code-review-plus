e886958b051e1bbb506a4a89932f934e2b1233a7
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2025-12-02 11:32:48 -0500

    Bump version: 0.14.0 → 0.15.0

--- code_review/__init__.py
+++ code_review/__init__.py
@@ -4,6 +4,6 @@ import code_review.plugins.linting.ruff.main  # This imports the ruff commands
 import code_review.review.main  # This imports the review commands
 from code_review.cli import cli
 
-__version__ = "0.14.0"
+__version__ = "0.15.0"
 if __name__ == "__main__":
     cli()
